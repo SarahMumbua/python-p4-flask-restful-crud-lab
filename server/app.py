d417e12bdd78c219d51ef76238e3213836d56b23
=SarahMumbua <mumbuasarah2030@gmail.com>
2023-07-02 00:52:15 +0300

    complete CRUD lab

--- server/app.py
+++ server/app.py
@@ -23,7 +23,6 @@ class Plants(Resource):
         return make_response(jsonify(plants), 200)
 
     def post(self):
-
         data = request.get_json()
 
         new_plant = Plant(
@@ -42,11 +41,37 @@ api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            return make_response(plant.to_dict(), 200)
+        else:
+            return make_response(jsonify({'message': 'Plant not found'}), 404)
+
+    def patch(self, id):
+        data = request.get_json()
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            if 'name' in data:
+                plant.name = data['name']
+            if 'image' in data:
+                plant.image = data['image']
+            if 'price' in data:
+                plant.price = data['price']
+            db.session.commit()
+            return make_response(plant.to_dict(), 200)
+        else:
+            return make_response(jsonify({'message': 'Plant not found'}), 404)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+            return make_response(jsonify({'message': 'Plant deleted'}), 200)
+        else:
+            return make_response(jsonify({'message': 'Plant not found'}), 404)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-        
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
